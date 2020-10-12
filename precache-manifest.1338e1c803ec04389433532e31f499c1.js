fbabc59ee84e09cb7a9c3002e9183a9fd6ca3c5e
Alim Ansari <alim.ans.771@gmail.com>
2020-10-12 21:45:49 +0530

    done

---
+++ precache-manifest.1338e1c803ec04389433532e31f499c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6c5e1407d327aa376b1feb7119413c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "193c5235286db94140d5",
+    "url": "/static/css/main.81ff03bf.chunk.css"
+  },
+  {
+    "revision": "087bf833c5a7f756fa6b",
+    "url": "/static/js/2.b43c7742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b43c7742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193c5235286db94140d5",
+    "url": "/static/js/main.dc91c302.chunk.js"
+  },
+  {
+    "revision": "de5486e3f83db0956805",
+    "url": "/static/js/runtime-main.513a6f46.js"
+  }
+]);
